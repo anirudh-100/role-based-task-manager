55ef3ef60f5a40c382425206e25c83f96185e5dc
anirudh-100 <anirudhrai1212@gmail.com>
2025-05-23 13:04:57 +0530

    frontend

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { createTask, getTasks, updateTaskStatus } from "../controllers/task.controller";
-import { verifyToken } from "../middleware/auth";
+import { createTask, getTasks, updateTaskStatus } from "../controllers/auth.controller";
+import { verifyToken } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
